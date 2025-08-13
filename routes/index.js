e247f21a76288c146e0a3f62f267d45be0d19393
mignrod <mignrod@gmail.com>
2025-08-13 14:56:29 -0400

    errors

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 router.use('/', require('./swagger'));
 router.use('/electronic', require('./electronic_device'));
